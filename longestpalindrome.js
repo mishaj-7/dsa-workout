f4788860237799d357a6ebde18b20b6c65c6a6a1
Mishaj__k <amstig100@gmail.com>
2024-06-04 19:33:14 +0530

    longestPalindrome

---
+++ longestpalindrome.js
@@ -0,0 +1,20 @@
+var longestPalindrome = function(s) {
+    const repet = new Map();
+    for(const str of s) {
+        repet.set(str, (repet.get(str) || 0) +1);
+    }
+    let ans =0;
+    let odd = false;
+    for (const count of repet.values()) {
+        ans += Math.floor(count / 2) * 2;
+    if (count % 2 === 1) {
+      odd = true;
+    }
+    }
+    if (odd) {
+    ans++;
+  }
+  return ans;
+};
+
+console.log(longestPalindrome("malayalam"))
